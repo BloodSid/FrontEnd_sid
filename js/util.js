0a1f581a5f13a4d356f98770a2af04bec82df712
89228 <892284811@qq.com>
2022-09-11 22:52:15 +0800

    演示：光的折射 修改重绘三角形的时机，不再用独立的响应函数

--- js/util.js
+++ js/util.js
@@ -10,6 +10,8 @@ createjs.Stage.prototype.enableDrag = function(...objs) {
 			// target 是事件的冒泡的起点元素，currentTarget 是当前冒泡到的元素，也是当前作用域中的 this
 			this.x = evt.stageX - this.offsetX;
 			this.y = evt.stageY - this.offsetY;
+			// target 若有需要在拖拽时进行的操作则执行
+			this.dragBinding && this.dragBinding();
 			stage.update();
 			// 停止冒泡，让该事件只触发一次，不会即触发子元素的移动又触发父元素的移动
 			evt.stopPropagation();
