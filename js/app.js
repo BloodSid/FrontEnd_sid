cf4f8aa849fdf55c61577ea6de0b58b800f95306
89228 <892284811@qq.com>
2022-09-11 09:50:58 +0800

    演示：光的折射，完成棱镜的制作

--- js/app.js
+++
@@ -1,4 +0,0 @@
-function init() {
-	console.log("Hi! " + (!!createjs ? "createJS is available." : "creagteJS is NOT available."));	
-	 
-}
---
+++ js/util.js
@@ -0,0 +1,18 @@
+createjs.Stage.prototype.enableDrag = function(...objs) {
+	var stage = this;
+	for (var obj of objs) {
+		if (!obj) continue;
+		obj.on("mousedown", function(evt) {
+			this.offsetX = evt.stageX - this.x;
+			this.offsetY = evt.stageY - this.y;
+		});
+		obj.on("pressmove", function(evt) {
+			// target 是事件的冒泡的起点元素，currentTarget 是当前冒泡到的元素，也是当前作用域中的 this
+			this.x = evt.stageX - this.offsetX;
+			this.y = evt.stageY - this.offsetY;
+			stage.update();
+			// 停止冒泡，让该事件只触发一次，不会即触发子元素的移动又触发父元素的移动
+			evt.stopPropagation();
+		});
+	}
+}
